27015cbec1276ad6efa3f1826d8b136e4b836a49
xittz <a.patseev@gmail.com>
2018-11-28 12:59:04 +0300

    fix writes, simpler version

--- app/models/User.scala
+++ app/models/User.scala
@@ -11,16 +11,16 @@ object User {
 
   implicit val userReads: Reads[User] = (
     Reads.pure(0: Long) and
-    (JsPath \ "username").read[String] and
-    (JsPath \ "password").read[String](minLength[String](6)) and
+    (__ \ "username").read[String] and
+    (__ \ "password").read[String](minLength[String](6)) and
     Reads.pure(false: Boolean)
   )(User.apply _)
 
-  implicit val userWrites: Writes[User] = (
-    (JsPath \ "id").write[Long] and
-    (JsPath \ "username").write[String] and
-    (JsPath \ "password").write[String] and
-    (JsPath \ "isDeleted").write[Boolean]
-  )(unlift(User.unapply))
+  implicit val userWrites: Writes[User] = (u: User) => Json.obj(
+      "id" -> u.id,
+      "username" -> u.username,
+      "password" -> u.password,
+      "isDeleted" -> u.isDeleted
+  )
 }
 
